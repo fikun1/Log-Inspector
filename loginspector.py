37de7794a64d9167f7ca9a2477415acf7610334a
DavidO <fikun@DavidOs-MacBook-Air.local>
2025-07-22 07:44:20 -0500

    My constants

--- loginspector.py
+++ loginspector.py
@@ -3,3 +3,15 @@ import argparse
 from collections import defaultdict
 from pathlib import Path
 
+IP_PATTERN = re.compile(
+    r'\b('
+    r'(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})\.){3}'
+    r'(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})\b'
+)
+
+FAILED_LOGIN_PATTERNS = [
+    r'Failed password',
+    r'authentication failure'
+]
+
+SENSITIVE_PATHS = ['/admin', '/wp-login', '/login', '/config']
